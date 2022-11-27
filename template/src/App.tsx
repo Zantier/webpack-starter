0027acea950f30b7f820a971a68d7c953ddc57c8
David Phillips <zantier.tasa@gmail.com>
2022-11-27 15:26:36 +0000

    React uses className, not class

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -9,7 +9,12 @@ import './App.css';
 
 export default () => {
   return (
+    {{#react}}
+    <div className="App">
+    {{/react}}
+    {{#preact}}
     <div class="App">
+    {{/preact}}
       hello
     </div>
   );
